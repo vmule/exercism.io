96d4e3d1d8ebc4e33e80d58ce606c05fcadc28cc
Vito Mulè <mulevito@gmail.com>
2014-11-10 10:27:12 +0000

    Update gigasecond.rb

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -3,8 +3,8 @@ require 'time'
 
 class Gigasecond
 
-  def self.from(gs)
-    date_in_sec = gs.to_time.to_i
+  def self.from(date)
+    date_in_sec = date.to_time.to_i
     gs_birtday_in_sec = date_in_sec + 10**9
     date_of_gs_birthday = Time.at(gs_birtday_in_sec).to_date
     return date_of_gs_birthday
